4ef4d5b61966322df6a39bc9c64f12f2e5b3cd5f
RaviRaj <RaviRaj@DESKTOP-MDEUDBT>
2023-06-23 01:48:35 +0530

    hi

---
+++ src/test/java/kins/Test5.java
@@ -0,0 +1,12 @@
+package kins;
+
+import org.testng.annotations.Test;
+
+public class Test5 {
+
+	@Test 
+	
+	public void tc5() {
+		System.out.println("hello python");
+	}
+}
---
+++ src/test/java/kins/Test6.java
@@ -0,0 +1,12 @@
+package kins;
+
+import org.testng.annotations.Test;
+
+public class Test6 {
+
+	
+	@Test
+	public void tc6() {
+		System.out.println("Heelo Anna");
+	}
+}
